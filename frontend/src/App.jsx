40071f4d7565e992da12c37c51879da10c16abac
deepak-pandey-10 <dp9136332@gmail.com>
2025-12-03 14:50:53 +0530

    commited

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 
-import SubjectDetails from './pages/SubjectDetails'
+
 import './App.css'
-import Signup from './pages/signup'
 import Login from './pages/login'
+import Signup from './pages/signup'
 import Dashboard from './pages/Dashboard'
+import SubjectDetails from './pages/SubjectDetails'
+
+
 
 function App() {
 
@@ -17,8 +20,8 @@ function App() {
         <Route path='/' element={<Navigate to="/login" replace />}></Route>
         <Route path='/login' element={<Login/>}></Route>
         <Route path='/signup' element={<Signup/>}></Route>
-        <Route path='/dashboard' element={<Dashboard />}></Route>
-        <Route path='/subject/:id' element={<SubjectDetails />}></Route>
+        <Route path='/dashboard' element={<Dashboard/>}></Route>
+        <Route path='/subject/:id' element={<SubjectDetails/>}></Route>
       </Routes>
     </BrowserRouter>
   )
